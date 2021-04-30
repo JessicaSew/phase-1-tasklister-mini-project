22db3877eca256649f9646bf3ca3e0f5c9fe0587
jessicasewell <sewell.jessica95@gmail.com>
2021-04-29 20:04:53 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+//document.addEventListener("DOMContentLoaded", () => { 
+    // your code here
+     //});
+  const taskForm = document.getElementById("create-task-form") 
+  const taskDescription = document.getElementById("new-task-description") 
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+const task = document.createElement('li')
+
+task.textContent = taskDescription.value
+taskList.appendChild(task)
+taskDescription.value = ""
+     })
+
+  
+
